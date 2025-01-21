29a6f4a7e712c521640039059f2aa3d8686ed387
Luke Moats <149527943+LMoats11@users.noreply.github.com>
2025-01-21 16:47:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const expresss = require('express')
+const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.static(_dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
            })
 
+app.set('view engine', 'ejs');
 
 //endpoint, middleware(s)
   app.get('/helloRender', function (req, res) {
     res.send
+  })
+
+  app.get('/nodemon', function (req, res) {
+    res.send('here it be');
+  })
+
+  app.get('/ejs', function (req, res) {
+    res.render('Great Heavens', 
+    {pageTitle: 'my cool ejs page'}
+  );
   })
