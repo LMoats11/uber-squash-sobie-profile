bf83111f3fc4295384661e08568a0c6fa6ac85a3
Moats <lmoats@una.edu>
2025-01-21 17:03:22 -0600

    jgtjtjt

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html')
+           })
+
+
+//endpoint, middleware(s)
+  app.get('/helloRender', function (req, res) {
+    res.send
+  })
+
+  app.get('/nodemon', function (req, res) {
+    res.send('here it be');
+  })
+
+  app.get('/ejs', function (req, res) {
+    res.render('Great Heavens', 
+    {pageTitle: 'my cool ejs page'}
+  );
+  })
+
+app.listen(
+    port, 
+    ()=> console.log(
+      `server is running on ... ${port}`
+      )
+    );
