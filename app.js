af40e4ecae4cd39395ba8cb8a2a45d23325a1a97
Luke Moats <149527943+LMoats11@users.noreply.github.com>
2025-01-21 16:00:37 -0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const expresss = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.static(_dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html')
+           })
+
+
+//endpoint, middleware(s)
+  app.listen(
+    port,
+    ()-> console.log("server is running on ...${port}'
+
+                     }
+    )
+
+    app.listen(3000)
