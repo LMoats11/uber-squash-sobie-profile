4abec281942b69954ec644fd3f58e1dcffb43ce6
Moats <lmoats@una.edu>
2025-01-28 16:46:32 -0600

    Mongo Code

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('dotenv').config()
+const app = express();
 const port = process.env.PORT || 3000;  
-const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri = process.env.MONGO_URI
+const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
+
+//console.log(uri);
+
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 //Create a MONGOClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
+ const client = new MongoClient(uri, {
+  serverApi: {version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,
-  }
-})
+    deprecationErrors: true,  }
+ });
 
+ async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+};
+
+run().catch(console.dir);
+
+//begin all my middlewares
 
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -25,7 +44,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
-})
+});
 
 app.post('/saveMyName', (req, res)=>{
   console.log('did we hit the endpoint??');
@@ -33,7 +52,7 @@ app.post('/saveMyName', (req, res)=>{
   console.log(req.body);
 
   res.redirect('/ejs');
-})
+});
 
 app.post('/saveMyName', (req, res)=>{
   console.log('did we hit the endpoint??');
@@ -43,10 +62,10 @@ app.post('/saveMyName', (req, res)=>{
   res.render('words', 
   {pageTitle: req.body.myName});
   
-  res.render('words', 
-  {theData: req.body});
+
   
-})
+}); 
+
 
 
 app.get('/ejs', async (req, res) => {
@@ -60,18 +79,18 @@ app.get('/ejs', async (req, res) => {
   res.prependListener('index', {
     ejsResult : result
   });
-}
+});
 
 
 app.get('/nodemon', function (req, res) {
   res.send('look ma, no kill node process then restart node then refresh browser...cool?');
 
-})
+});
 
 //endpoint, middleware(s)
-app.get('/helloRender', function (req, res) {
-  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
-})
+// app.get('/helloRender', function (req, res) {
+//   res.send('Hello Express from Real World<br><a href="/">back to home</a>')
+// })
 
 app.listen(
   port, 
