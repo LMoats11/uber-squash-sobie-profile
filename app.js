9eec87802027c3c501b3ddf92b5223f5d262922d
Luke Moats <luke.moats@gmail.com>
2025-02-26 18:24:52 -0600

    code fix

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const mongoCollection = client.db("lukeSobieProfile").collection("lukeSobieBlog"
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', async (req, res) => {
   let results = await mongoCollection.find({}).toArray();
